20d6f71659565f7b0dc7eeb289a1632fd9675670
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-11 14:31:26 +0100

    Add sparse cp_als test

--- test/sptensorTest.jl
+++ test/sptensorTest.jl
@@ -5,3 +5,6 @@ dense = Array(sp)
 
 println("... Testing sparse and dense tensor product equivalence.")
 @test ttt(sp,sp) == ttt(dense,dense)
+
+println("... Testing sparse tensor decomposition")
+@test cp_als(reduce(ttt,SparseTensor.([[0, 0, 1],[0, 1, 0],[1, 0, 0]])),1) == ktensor([[0 0 1],[0 1 0],[1 0 0]].|>permutedims .|> x->x .|> Float64)
